0b417808f4917314af9c850fa4386e8b768f9cf3
Rishi-0007 <rishikumarnayak9@gmail.com>
2025-06-25 16:57:38 +0530

    Setup Auth layouts,Social Auth Form, Next Auth, OAuth with Google and Github

---
+++ app/(root)/page.tsx
@@ -0,0 +1,24 @@
+import { auth, signOut } from "@/auth";
+import ROUTES from "@/constants/route";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <form
+        className="px-10 pt-[100px] "
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGNIN });
+        }}
+      >
+        <button type="submit">Log out</button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
